0034e995971886b355c2ac7db55224552a6b75d4
mpalomera <miguel.palomera@gmail.com>
2017-11-02 19:43:46 -0600

    total

--- thread/main.c
+++ thread/main.c
@@ -2,13 +2,16 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define NUM_THREADS 100
+#define NUM_THREADS 10000
 
-void *PrintHello(void *threadid)
+unsigned int total;
+
+void *add100(void *threadid)
 {
   long tid;
   tid = (long)threadid;
-  printf("Hello World! It's me, thread #%ld!\n", tid);
+  printf("Adding 100 #%ld!\n", tid);
+  total += 100;
   pthread_exit(NULL);
 }
 
@@ -17,17 +20,23 @@ int main(int argc, char *argv[])
   pthread_t threads[NUM_THREADS];
   int rc;
   long t;
+  total = 0;
+  printf("Initial total = %d \n", total);
   for (t = 0; t < NUM_THREADS; t++)
   {
     printf("In main: creating thread %ld\n", t);
-    rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+    rc = pthread_create(&threads[t], NULL, add100, (void *)t);
     if (rc)
     {
       printf("ERROR; return code from pthread_create() is %d\n", rc);
       exit(-1);
     }
   }
-
+  for (t = 0; t < NUM_THREADS; t++)
+  {
+    pthread_join(threads[t], NULL);
+  }
+  printf("Final total = %d \n", total);
   /* Last thing that main() should do */
   pthread_exit(NULL);
 }
