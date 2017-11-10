484fc213406adef3eec6910273b361772ecc2182
mpalomera <miguel.palomera@gmail.com>
2017-11-09 19:13:27 -0600

    phil with chairs

--- thread/main.c
+++ thread/main.c
@@ -9,30 +9,39 @@
 
 unsigned int total;
 //sem_t forks[NUM_THREADS];
+//sem_t chairs
 dispatch_semaphore_t forks[NUM_THREADS];
-void thinking(int n) {
-  sleep(rand()%n);
+dispatch_semaphore_t chairs;
+void thinking(int n)
+{
+  sleep(rand() % n);
 }
-void eating(int n) {
-  sleep(rand()%n);
+void eating(int n)
+{
+  sleep(rand() % n);
 }
 
 void *lifeStyle(void *threadid)
 {
   long tid;
   tid = (long)threadid;
-  while(1) {
+  while (1)
+  {
     thinking(3); //
     printf("I am phil %lu, I am hungry \n", tid);
     //sem_wait(&forks[tid]);
     //sem_wait(&forks[(tid + 1 )%NUM_THREADS]);
+    //sem_wait(&chairs);
+    dispatch_semaphore_wait(chairs,DISPATCH_TIME_FOREVER);
+    dispatch_semaphore_wait(forks[(tid + 1) % NUM_THREADS], DISPATCH_TIME_FOREVER);
     dispatch_semaphore_wait(forks[tid], DISPATCH_TIME_FOREVER);
-    dispatch_semaphore_wait(forks[(tid + 1 )%NUM_THREADS], DISPATCH_TIME_FOREVER);
     eating(4);
     dispatch_semaphore_signal(forks[tid]);
-    dispatch_semaphore_signal(forks[(tid + 1 )%NUM_THREADS]);
+    dispatch_semaphore_signal(forks[(tid + 1) % NUM_THREADS]);
+    dispatch_semaphore_signal(chairs);
     //sem_post(&forks[tid]);
     //sem_post(&forks[(tid + 1 )%NUM_THREADS]);
+    //sem_post(&chairs);
     printf("I am phil %lu, I am thinking \n", tid);
   }
 }
@@ -42,11 +51,14 @@ int main(int argc, char *argv[])
   pthread_t threads[NUM_THREADS];
   int rc;
   long t;
-  
-  for (t = 0; t < NUM_THREADS; t++) {
+
+  for (t = 0; t < NUM_THREADS; t++)
+  {
     //sem_init(&forks[t],0,1);
     forks[t] = dispatch_semaphore_create(1);
   }
+  //sem_init(&chairs,0,NUM_THREADS/2);
+  chairs = dispatch_semaphore_create(NUM_THREADS / 2);
   for (t = 0; t < NUM_THREADS; t++)
   {
     printf("In main: creating thread %ld\n", t);
@@ -61,7 +73,7 @@ int main(int argc, char *argv[])
   {
     pthread_join(threads[t], NULL);
   }
- 
+
   /* Last thing that main() should do */
   pthread_exit(NULL);
 }
